6e8e44abfa13f5f59978fa044eda2b32d2bc49cb
shobhit-joshi-8 <87273043+shobhit-joshi-8@users.noreply.github.com>
2023-04-09 12:18:14 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ReactCaraousel from "./components/ReactCaraousel";
+
+function App() {
+  return <ReactCaraousel />;
+}
+
+export default App;
